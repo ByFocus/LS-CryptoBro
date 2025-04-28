d62b521fa74effc591a165e06a8b4f0905fb12d3
sergiGN01 <sergign333@gmail.com>
2025-04-28 09:14:24 +0200

    Merging everything

---
+++ src/Persistance/PersistanceExceptions/ConfigurationFileError.java
@@ -0,0 +1,7 @@
+package Persistance.PersistanceExceptions;
+
+public class ConfigurationFileError extends PersistanceException {
+    public ConfigurationFileError(String message) {
+        super(message);
+    }
+}
