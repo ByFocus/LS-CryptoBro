64e63c98c05c1f15f91f7c7b6b1f2d3a1594964a
sergiGN01 <sergign333@gmail.com>
2025-04-30 19:21:48 +0200

    Added the logic of the historical values, merging it with the initialitzation of the bots. The historics are a hasMap, where the keys are the CryptoNames, and the values are Queues of doubles (the prices). The MarketManager, is now an extension from thread, that interrupts every 5 seconds, and gets the values for each crypto, and add its to the QUEUE, if the QUEUE has already MAX_VALUES, it removes the oldest one.

--- src/Business/CryptoManager.java
+++ src/Business/CryptoManager.java
@@ -1,12 +1,20 @@
 package Business;
 
 import Business.Entities.Crypto;
+import Persistance.CryptoDAO;
+import Persistance.CryptoSQLDAO;
 
 import java.util.List;
 
 public class CryptoManager {
     public List<Crypto> getAllCryptos() {
-        return null;
+        CryptoDAO cryptoDA0 =new CryptoSQLDAO();
+        return cryptoDA0.getAllCryptos();
+    }
+
+    public Crypto getCryptoByName(String cryptoName) {
+        CryptoDAO cryptoDA0 =new CryptoSQLDAO();
+        return cryptoDA0.getCryptoByName(cryptoName);
     }
 
     public synchronized void makeTransaction(String cryptoName, int units) {
