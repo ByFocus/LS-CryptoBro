7b133dafa527e21dc04d2a3322957ebed0406ed1
sergiGN01 <sergign333@gmail.com>
2025-05-14 12:07:35 +0200

    Faltaba el dispose en una función, además se ha cambiado la lógica para que después de registrar-se te salga el panel de logearte. Y también he puesto funciones privadas, para que el switch quede más limpio

--- src/Presentation/Controllers/AccountViewController.java
+++ src/Presentation/Controllers/AccountViewController.java
@@ -46,73 +46,94 @@ public class AccountViewController implements ActionListener, EventListener {
     public void actionPerformed(ActionEvent e) {
         switch (e.getActionCommand()) {
             case StartFrame.USER_LOGIN:
-                if(startView.getNameInput().isEmpty() || startView.getPasswordInput().isEmpty()) {
-                    ErrorDisplayer.displayError(ERROR_EMPTY_FIELD);
-                } else {
-                    String userName = startView.getNameInput();
-                    String password = startView.getPasswordInput();
-                    if (userName.equalsIgnoreCase("admin")) {
-                        try {
-                            startView.reset();
-                            AccountManager.getInstance().adminAccess(password);
-                            ApplicationController.getInstance().userConfirmed(true);
-                        } catch (BusinessExeption e2) {
-                            ErrorDisplayer.displayError(e2.getMessage());
-                        }
-                    }
-                    else {
-                        try  {
-                            User user =AccountManager.getInstance().loginUser(userName, password);
-                            //TODO: SE TIENE QUE HACER ALGO CON ESTE USUARIO PARA PRINTAR SU INFO
-                            startView.reset();
-                            ApplicationController.getInstance().userConfirmed(false);
-                        } catch (BusinessExeption e1) {
-                            ErrorDisplayer.displayError(e1.getMessage());
-                        }
-                    }
-                }
+                loginUser();
                 break;
 
             case StartFrame.USER_REGISTER:
-                String userName = startView.getNameInput();
-                String password = startView.getPasswordInput();
-                String email = startView.getEmailInput();
-
-                if(userName.isEmpty() || password.isEmpty() || email.isEmpty()) {
-                    ErrorDisplayer.displayError(ERROR_EMPTY_FIELD);
-                } else {
-                    try {
-                        AccountManager am = AccountManager.getInstance();
-                        am.registerUser(userName, email, password);
-                        User user =AccountManager.getInstance().loginUser(userName, password);
-                        //TODO: SE TIENE QUE USAR EL USUARIO
-                        startView.reset();
-                        startView.dispose();
-                        ApplicationController.getInstance().userConfirmed(false);
-                    } catch (BusinessExeption e1) {
-                        ErrorDisplayer.displayError(e1.getMessage());
-                    }
-                }
+                registerUser();
                 break;
 
             case StartFrame.SWITCH_LOGIN:
-                startView.switchView(StartFrame.LOGIN_VIEW);
-                startView.reset();
+                switchToLoginView();
                 break;
 
             case StartFrame.SWITCH_REGISTER:
-                startView.switchView(StartFrame.REGISTER_VIEW);
-                startView.reset();
+                switchToRegisterView();
                 break;
-
             case UserPopUp.USER_LOGOUT:
-                userView.dispose();
-                ApplicationController.getInstance().close();
-                startView.setVisible(true);
+                userLogOut();
                 break;
         }
     }
 
+    private void switchToRegisterView() {
+        startView.switchView(StartFrame.REGISTER_VIEW);
+        startView.reset();
+    }
+
+    private void switchToLoginView() {
+        startView.switchView(StartFrame.LOGIN_VIEW);
+        startView.reset();
+    }
+
+    private void registerUser() {
+        String userName = startView.getNameInput();
+        String password = startView.getPasswordInput();
+        String email = startView.getEmailInput();
+
+        if(userName.isEmpty() || password.isEmpty() || email.isEmpty()) {
+            ErrorDisplayer.displayError(ERROR_EMPTY_FIELD);
+        } else {
+            try {
+                AccountManager am = AccountManager.getInstance();
+                am.registerUser(userName, email, password);
+                //User user =AccountManager.getInstance().loginUser(userName, password);
+                //TODO: SE TIENE QUE USAR EL USUARIO o igual que se logee
+                //startView.reset();
+                //startView.dispose();
+                //ApplicationController.getInstance().userConfirmed(false);
+                switchToRegisterView();
+            } catch (BusinessExeption e1) {
+                ErrorDisplayer.displayError(e1.getMessage());
+            }
+        }
+    }
+
+    private void loginUser() {
+        if(startView.getNameInput().isEmpty() || startView.getPasswordInput().isEmpty()) {
+            ErrorDisplayer.displayError(ERROR_EMPTY_FIELD);
+        } else {
+            String userName = startView.getNameInput();
+            String password = startView.getPasswordInput();
+            if (userName.equalsIgnoreCase("admin")) {
+                try {
+                    AccountManager.getInstance().adminAccess(password);
+                    ApplicationController.getInstance().userConfirmed(true);
+                    startView.reset();
+                    startView.dispose();
+                } catch (BusinessExeption e2) {
+                    ErrorDisplayer.displayError(e2.getMessage());
+                }
+            }
+            else {
+                try  {
+                    User user =AccountManager.getInstance().loginUser(userName, password);
+                    //TODO: SE TIENE QUE HACER ALGO CON ESTE USUARIO PARA PRINTAR SU INFO
+                    startView.reset();
+                    startView.dispose();
+                    ApplicationController.getInstance().userConfirmed(false);
+                } catch (BusinessExeption e1) {
+                    ErrorDisplayer.displayError(e1.getMessage());
+                }
+            }
+        }
+    }
+
+    private void userLogOut() {
+        userView.dispose();
+        ApplicationController.getInstance().close();
+        startView.setVisible(true);
+    }
     public void update(EventType event) {
 
     }
