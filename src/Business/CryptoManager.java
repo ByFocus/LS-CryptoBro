327efd2a8e27a160d493fb100f00ec47f99a47e9
sergiGN01 <sergign333@gmail.com>
2025-05-19 10:17:03 +0200

    ahora se actualizan los valores de las cryptos

--- src/Business/CryptoManager.java
+++ src/Business/CryptoManager.java
@@ -53,6 +53,7 @@ public class CryptoManager{
             CryptoDAO cryptoDA0 =new CryptoSQLDAO();
             double newPrice = cryptoDA0.getCryptoCurrentPrice(cryptoName)*priceMultiplier;
             cryptoDA0.updateCryptoPrice(cryptoName, newPrice);
+            MarketManager.getMarketManager().notify(EventType.CRYPTO_VALUES_CHANGED);
         } catch (PersistanceException e) {
             throw new DataPersistanceError(e.getMessage());
         }
--- src/Presentation/View/Tables/CryptoTableModel.java
+++ src/Presentation/View/Tables/CryptoTableModel.java
@@ -13,7 +13,7 @@ public class CryptoTableModel extends AbstractTableModel {
         this.cryptos = cryptos;
     }
 
-    public void setData(List<Crypto> cryptos) {
+    public synchronized void setData(List<Crypto> cryptos) {
         this.cryptos.clear();
         this.cryptos.addAll(cryptos);
     }
@@ -29,15 +29,18 @@ public class CryptoTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        Crypto crypto = cryptos.get(rowIndex);
-        switch (columnIndex) {
-            case 0: return crypto.getName();
-            case 1: return crypto.getCategory();
-            case 2: return crypto.getCurrentPrice();
-            case 3: return "-";
-            case 4: return "%";
-            default: return null;
+        if (rowIndex <= cryptos.size()) {
+            Crypto crypto = cryptos.get(rowIndex);
+            switch (columnIndex) {
+                case 0: return crypto.getName();
+                case 1: return crypto.getCategory();
+                case 2: return crypto.getCurrentPrice();
+                case 3: return "-";
+                case 4: return "%";
+                default: return null;
+            }
         }
+        return null;
     }
 
     @Override
