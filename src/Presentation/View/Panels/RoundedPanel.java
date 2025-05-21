3555b0beafc41376a27543260fc1268fe6940817
mario aguado <mario.aguado123@outlook.com>
2025-05-21 09:32:27 +0200

    Added RoundedPanel class and updated the design of UserPopUp

---
+++ src/Presentation/View/Panels/RoundedPanel.java
@@ -0,0 +1,25 @@
+package Presentation.View.Panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedPanel extends JPanel {
+    private int cornerRadius;
+
+    public RoundedPanel(int radius) {
+        super();
+        this.cornerRadius = radius;
+        setOpaque(false); // Needed for transparency
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, cornerRadius, cornerRadius);
+        g2.setColor(getForeground());
+        g2.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, cornerRadius, cornerRadius);
+    }
+}
--- src/Presentation/View/Popups/UserPopUp.java
+++ src/Presentation/View/Popups/UserPopUp.java
@@ -1,24 +1,22 @@
 package Presentation.View.Popups;
 
-import Business.Entities.User;
-
+import Presentation.View.Panels.RoundedPanel;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class UserPopUp extends JFrame {
-    //Constantes con los eventos
+    // Event constants
     public static final String USER_LOGOUT = "USER_LOGOUT";
     public static final String USER_ERASE_ACCOUNT = "USER_ERASE_ACCOUNT";
 
-    //Constantes con titulos, enlaces a fotos y textos
-    public static final String FONT_TITLE = "Serif";
+    // Fonts and resources
+    public static final String FONT_TITLE = "Open Sans";
     public static final String FONT_TEXT = "Arial";
-
     public static final String FRAME_TITLE = "User Profile";
     public static final String userProfileImgURL = "imgs/usuario.png";
 
-    //Atributos
+    // UI attributes
     private final JLabel userNameLabel;
     private final JLabel userEmailLabel;
     private JLabel userBalanceLabel;
@@ -26,24 +24,39 @@ public class UserPopUp extends JFrame {
 
     private JButton logOutButton;
     private JButton deleteAccountButton;
+    private JButton changePasswordButton;
 
     public UserPopUp(String userName, String userEmail, String userBalance, String userPassword, boolean admin) {
         configureFrame();
 
-        this.userNameLabel = new JLabel(userName);;
+        this.userNameLabel = new JLabel(userName);
         this.userBalanceLabel = new JLabel("Balance: " + userBalance);
         this.userEmailLabel = new JLabel("Email: " + userEmail);
-        this.userPasswordLabel = new JLabel("Password: " + userPassword);;
+        this.userPasswordLabel = new JLabel("Password: " + userPassword);
 
         configureUserProfile();
-        confiureActions(admin);
+        configureActions(admin);
     }
 
     private void configureUserProfile(){
+        setSize(600, 500);
+
         Container c = getContentPane();
         c.setBackground(new Color(244, 233, 205));
-        c.setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        c.setLayout(new BoxLayout(c, BoxLayout.Y_AXIS));
 
+        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        topPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0));
+        topPanel.setOpaque(false);
+
+        logOutButton = new JButton("Log Out");
+        logOutButton.setFont(new Font(FONT_TEXT, Font.ITALIC | Font.BOLD, 21));
+        logOutButton.setBackground(new Color(3, 25, 38));
+        logOutButton.setForeground(Color.WHITE);
+        logOutButton.setActionCommand(USER_LOGOUT);
+
+        topPanel.add(logOutButton);
+        c.add(topPanel);
 
         Image userProfileImg = new ImageIcon(userProfileImgURL).getImage().getScaledInstance(150, 150, Image.SCALE_SMOOTH);
         JLabel userProfile = new JLabel(new ImageIcon(userProfileImg));
@@ -58,12 +71,12 @@ public class UserPopUp extends JFrame {
 
         JPanel userInfoPanel = new JPanel(new BorderLayout());
         userInfoPanel.setOpaque(false);
-        userInfoPanel.setBorder(BorderFactory.createEmptyBorder(0, 30, 0, 30));
+        userInfoPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 0, 30));
 
-        JPanel textPanel = new JPanel(new GridBagLayout());
+        JPanel textPanel = new RoundedPanel(20); // 20 is the border radius
         textPanel.setBackground(new Color(70, 129, 137));
-
         textPanel.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.weightx = 1;
@@ -71,59 +84,44 @@ public class UserPopUp extends JFrame {
 
         gbc.gridx = 0;
         gbc.gridy = 0;
-
-        userBalanceLabel.setFont(new Font(FONT_TEXT, Font.BOLD, 18));
-
-        textPanel.add(userBalanceLabel, gbc);
-
-        gbc.gridy = 1;
-
         userEmailLabel.setFont(new Font(FONT_TEXT, Font.BOLD, 18));
-
+        userEmailLabel.setForeground(Color.white);
         textPanel.add(userEmailLabel, gbc);
 
-        gbc.gridy = 2;
 
-        userPasswordLabel.setFont(new Font(FONT_TEXT, Font.BOLD, 18));
-
-        textPanel.add(userPasswordLabel, gbc);
-
-        userInfoPanel.add(textPanel);
+        userInfoPanel.add(textPanel, BorderLayout.NORTH);
         c.add(userInfoPanel);
     }
 
-    private void confiureActions(boolean admin){
-        //Boton para cerrar sesion
+    private void configureActions(boolean admin){
+        // Bottom panel for other action buttons
         JPanel actionPanel = new JPanel();
-
-        logOutButton = new JButton("Log Out");
-        logOutButton.setFont(new Font(FONT_TEXT, Font.ITALIC | Font.BOLD, 21));
-        logOutButton.setBackground(new Color(3, 25, 38));
-        logOutButton.setForeground(Color.WHITE);
-        logOutButton.setActionCommand(USER_LOGOUT);
-
-        actionPanel.add(logOutButton);
         actionPanel.setOpaque(false);
         actionPanel.setBorder(BorderFactory.createEmptyBorder(30, 0, 0, 0));
 
+        // Only add delete and change password if not admin
         if (!admin) {
             deleteAccountButton = new JButton("Delete Account");
             deleteAccountButton.setFont(new Font(FONT_TEXT, Font.ITALIC | Font.BOLD, 21));
-            deleteAccountButton.setBackground(new Color(119, 172, 162));
+            deleteAccountButton.setBackground(new Color(213, 0, 0));
             deleteAccountButton.setForeground(Color.WHITE);
             deleteAccountButton.setActionCommand(USER_ERASE_ACCOUNT);
-
             actionPanel.add(deleteAccountButton);
+
+            changePasswordButton = new JButton("Change Password");
+            changePasswordButton.setFont(new Font(FONT_TEXT, Font.BOLD, 21));
+            changePasswordButton.setBackground(new Color(70, 129, 137));
+            changePasswordButton.setForeground(Color.WHITE);
+            // You may want to set a different action command for this button
+            changePasswordButton.setActionCommand("USER_CHANGE_PASSWORD");
+            actionPanel.add(changePasswordButton);
         }
 
         getContentPane().add(actionPanel);
-
     }
 
     private void configureFrame() {
-        pack();
         setTitle(FRAME_TITLE);
-        setSize(400, 600);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
     }
@@ -132,6 +130,9 @@ public class UserPopUp extends JFrame {
         logOutButton.addActionListener(listener);
         if (deleteAccountButton != null) {
             deleteAccountButton.addActionListener(listener);
+        }
+        if (changePasswordButton != null) {
+            changePasswordButton.addActionListener(listener);
         }
     }
 
